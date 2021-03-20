4c64fa75285a3dfb75e3ad9966227204776fb84b
shahryar haghighi fard <shahryarhaghighifard@shahryars-MacBook-Pro.local>
2021-03-20 16:34:28 -0400

    implemented the toggle menu's functionality and animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,32 @@
 /* Sherweb - Test intégration */
 
 function toggleMenu(menu) {
-  let menu2 = document.getElementById("menu-change");
-  console.log(menu2.style.display == "");
-  if (menu2.style.display === "none" || menu2.style.display == "") {
-    menu2.style.display = "block";
-  } else {
-    menu2.style.display = "none";
-  }
+  // let menu2 = document.getElementById("menu-change");
+  // if (menu2.style.display === "none" || menu2.style.display == "") {
+  //   menu2.style.display = "flex";
+  // } else {
+  //   menu2.style.display = "none";
+  // }
   menu.classList.toggle("change");
+  let navmenu = document.querySelector(".open-menu");
+  let menuIsClosed = !navmenu.classList.contains("opened");
+  if (menuIsClosed) {
+    navmenu.classList.add("opened");
+  } else {
+    navmenu.classList.remove("opened");
+  }
 }
+
+// let menu = document.querySelector("#menu-change");
+// let menuButton = document.querySelector(".menu");
+
+// // add an event listener to listen for clicks on the menu button
+// menuButton.addEventListener("click", function () {
+//   let menuIsClosed = !menu.classList.contains("opened"); // we know the menu is closed if it doesn't have the ".opened" class
+
+//   if (menuIsClosed) {
+//     menu.classList.add("opened change");
+//   } else {
+//     menu.classList.remove("opened change");
+//   }
+// });
